2cdd241f4867c235aad5e55ae1cccfa2ba822102
0-BSCode <bryanipsanchez@gmail.com>
2024-05-31 22:19:53 +0800

    Add rule to remove unused imports

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,26 @@
+import unusedImports from "eslint-plugin-unused-imports";
 import eslintPluginSvelte from 'eslint-plugin-svelte';
+
 export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginSvelte.configs['flat/recommended'],
   ...eslintPluginSvelte.configs['flat/prettier'],
   {
+    plugins: {"unused-imports": unusedImports},
     rules: {
-      // override/add rules settings here, such as:
-      // 'svelte/rule-name': 'error'
+      "svelte/valid-compile": ["error", { ignoreWarnings: true }],
+      "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+          "warn",
+          {
+              "vars": "all",
+              "varsIgnorePattern": "^_",
+              "args": "after-used",
+              "argsIgnorePattern": "^_",
+          },
+      ]
     },
     ignores: [
         "node_modules",
