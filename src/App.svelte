b0787e7c896d7cd0361b21b430b2bfc53ea1114c
0-BSCode <bryanipsanchez@gmail.com>
2024-06-01 23:01:33 +0800

    File upload

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,64 @@
 <script lang="ts">
+  import Dropzone from "svelte-file-dropzone";
+
+  let acceptedFile = undefined;
+  let rejectedFile: File | undefined = undefined;
+  let showFile: boolean = false;
+  let file: HTMLImageElement;
+
+  interface DropzoneEvent {
+    detail: {
+      acceptedFiles: File[];
+      fileRejections: File[];
+    };
+  }
+
+  function handleFilesSelect(e: DropzoneEvent) {
+    const { acceptedFiles, fileRejections } = e.detail;
+    acceptedFile = acceptedFiles[0];
+    rejectedFile = fileRejections[0];
+
+    if (acceptedFile) {
+      showFile = true;
+
+      const reader = new FileReader();
+      reader.addEventListener("load", () => {
+        file.setAttribute("src", reader.result as string);
+      });
+      reader.readAsDataURL(acceptedFile);
+    } else {
+      showFile = false;
+    }
+  }
+
+  function handleDeleteFile() {
+    acceptedFile = undefined;
+    showFile = false;
+  }
 </script>
 
-<main>
-  <h1 class="text-red-500">Vite + Svelte</h1>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+<main class="flex">
+  <nav class="m-8 flex w-1/4 flex-col">
+    <Dropzone
+      on:drop={handleFilesSelect}
+      accept={"image/*"}
+      multiple={false}
+      containerStyles="padding: 0;"
+    >
+      {#if showFile}
+        <div class="relative">
+          <p>Test</p>
+          <img bind:this={file} class="w-full" src="" alt="Preview" />
+        </div>
+      {:else}
+        <span>Image Preview</span>
+      {/if}
+    </Dropzone>
+    <button
+      on:click={handleDeleteFile}
+      class="z-50 my-2 w-full border-2 border-red-200 text-center"
+      >Delete</button
+    >
+  </nav>
+  <body class="flex flex-grow flex-col bg-slate-400"> </body>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
