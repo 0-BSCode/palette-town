8ffd18595584c511780d2379c4ebe1c7f69bea43
0-BSCode <bryanipsanchez@gmail.com>
2024-06-02 20:56:48 +0800

    Tweak color extractor options and prevent image overflow

--- src/App.svelte
+++ src/App.svelte
@@ -4,11 +4,11 @@
   import type { ColorI } from "./types/interfaces/ColorInterface";
   import calculateRGBBrightness from "./utils/calculateRGBBrightness";
 
-  let acceptedFile = undefined;
+  let acceptedFile: File | undefined = undefined;
   let rejectedFile: File | undefined = undefined;
-  let showFile: boolean = false;
   let file: HTMLImageElement;
   let colors: ColorI[] = [];
+  $: showFile = acceptedFile !== undefined;
 
   interface DropzoneEvent {
     detail: {
@@ -23,42 +23,47 @@
     rejectedFile = fileRejections[0];
 
     if (acceptedFile) {
-      showFile = true;
-
       const reader = new FileReader();
       reader.addEventListener("load", async () => {
         file.setAttribute("src", reader.result as string);
-        const result = await extractColorsFromSrc(file.src);
+        const result = await extractColorsFromSrc(file.src, {
+          // TODO: Allow user to control settings
+          distance: 0.2,
+        });
         colors = result.map((r) => ({
           ...r,
           isDark: calculateRGBBrightness(r.red, r.green, r.blue) < 128,
         }));
       });
       reader.readAsDataURL(acceptedFile);
-    } else {
-      showFile = false;
     }
   }
 
   function handleDeleteFile() {
     acceptedFile = undefined;
-    showFile = false;
     colors = [];
   }
 </script>
 
-<main class="flex">
-  <nav class="m-8 flex w-1/4 flex-col">
+<main class="flex h-screen min-h-fit">
+  <nav class="m-8 flex w-1/4 flex-col items-center gap-2">
+    <h1>Image</h1>
     <Dropzone
       on:drop={handleFilesSelect}
       accept={"image/*"}
       multiple={false}
-      containerStyles="padding: 0;"
+      containerStyles="padding-block: 0.5rem; display: flex; flex-direction: column; flex: 2; align-items: center; justify-content: center; border: 2px dashed #ccc; border-radius: 5px; width: 100%;"
     >
       {#if showFile}
-        <div class="relative">
-          <p>Test</p>
-          <img bind:this={file} class="max-h-fit w-full" src="" alt="Preview" />
+        <div
+          class="relative flex h-full flex-1 flex-col justify-center overflow-y-hidden"
+        >
+          <img
+            bind:this={file}
+            class="h-96 object-contain"
+            src=""
+            alt="Preview"
+          />
         </div>
       {:else}
         <span>Image Preview</span>
@@ -70,14 +75,19 @@
       >Delete</button
     >
   </nav>
-  <body class="flex flex-grow flex-col bg-slate-400">
-    {#each colors as color}
-      <div
-        class="p-3"
-        style={`background-color: ${color.hex}; color: ${color.isDark ? "white" : "black"}`}
-      >
-        {color.hex}
-      </div>
-    {/each}
+  <body class="flex h-full w-3/4 flex-grow flex-col bg-slate-400 p-3">
+    <!-- Color list -->
+    <section class="flex flex-wrap gap-2">
+      {#each colors as color}
+        <div
+          class="flex aspect-square w-fit items-center rounded-md p-5"
+          style={`background-color: ${color.hex}; color: ${color.isDark ? "white" : "black"}`}
+        >
+          <p>
+            {color.hex}
+          </p>
+        </div>
+      {/each}
+    </section>
   </body>
 </main>
